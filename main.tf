09effec8171e639da7ad51f926fbab1485157ea1
Eugen2712 <eugeniu.plotnic@gmail.com>
2025-04-06 21:06:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,8 @@ resource "aws_instance" "web_server" {
               #!/bin/bash
               amazon-linux-extras install epel
               yum update -y
+              PUBLIC_IP=$(curl -s https://api.ipify.org)
+              curl -X GET "https://devops:11653bd900a61a61152c8caab3b8f24def@jenkins-ops.portnov.com/job/eugen_ansible/buildWithParameters?token=Abc123456&MANAGED_HOST=$PUBLIC_IP"
               EOF
 }
 
